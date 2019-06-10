ca114092254f1976a4db04d448a205a0f17c6da5
tiagocarvalhido <emailparajogospc@gmail.com>
2019-06-10 08:43:40 +0100

    comentarios

--- HelpMeParkIOS/Comentario.swift
+++ HelpMeParkIOS/Comentario.swift
@@ -18,6 +18,7 @@ class Comentario: UIViewController {
     
     let reff = Database.database().reference()
     
+     var dbHandle: DatabaseHandle?
     
 
     @IBOutlet weak var nomeUserComent: UILabel!
@@ -36,11 +37,77 @@ class Comentario: UIViewController {
     @IBOutlet weak var obsvacoes: UITextField!
     
     @IBOutlet weak var dataAtual: UILabel!
+    
+    @IBOutlet weak var lblRatingBar: UILabel!
+    
     override func viewDidLoad() {
         let refff =  Database.database().reference().child("Parques").child(ParqueList)
         print("PRINT LIST")
-        print(ParqueList)
+        
+        
+        dbHandle = refff.child("comentario").observe(.value,  with:{(snapshot) in
+            let nomeUser:String? = snapshot.childSnapshot(forPath:"nomeUser").value as? String
+           
+            self.nomeUserComent.text = (nomeUser)
+            
+            let data:String? = snapshot.childSnapshot(forPath:"data").value as? String
+            
+            self.dataComent.text = (data)
+            
+            let disponibilidade:Double? = snapshot.childSnapshot(forPath:"disponibilidade").value as? Double
+            
+            self.dispComent.rating = Double(disponibilidade!) as! Double
+            
+            print( disponibilidade)
+            
+            let text_coment:String? = snapshot.childSnapshot(forPath:"text_Coment").value as? String
+        
+            self.obsComent.text = (text_coment)
+            
+            
+            self.dispComent.settings.starSize = 27
+            
+            
+            avaliar()
+            
+            //self.lblRatingBar.sizeToFit()
+            
+        })
+        
+        
+        func avaliar(){
+            
+            ratingBar.didTouchCosmos = {rating in
+                
+                
+                switch self.ratingBar.rating {
+                case 1:
+                    self.lblRatingBar.text = "Parque Vazio"
+                    
+                case 2:
+                     self.lblRatingBar.text = "Quase Vazio"
+                case 3:
+                     self.lblRatingBar.text = "Meio Cheio"
+                case 4:
+                     self.lblRatingBar.text = "Parque quase cheio"
+                case 5:
+                     self.lblRatingBar.text = "Parque Cheio"
+                    
+                default:
+                    self.lblRatingBar.text = ""
+                }
+            }
+        }
+        
+        
+           
+            
+        
 
+            
+            
+            
+        
         
         
         getCurrentDateTime()
@@ -56,13 +123,15 @@ class Comentario: UIViewController {
         self.dataAtual.text = str
         
     }
+    
     @IBAction func addComentario(_ sender: Any) {
     let email = Auth.auth().currentUser?.email
     self.nomeUserAtual.text = email
 
-        
+   
+   
        
-        let data1 = ["disponibilidade": ratingBar.rating,"nomeUser": nomeUserAtual.text!, "text_Coment": obsvacoes.text!, "data": dataAtual.text!] as [String : Any]
+        let data1 = ["disponibilidade":ratingBar.rating  ,"nomeUser": nomeUserAtual.text!, "text_Coment": obsvacoes.text!, "data": dataAtual.text!] as [String : Any]
         
         self.reff.child("Parques").child(parque).child("comentario").setValue(data1)
         }
